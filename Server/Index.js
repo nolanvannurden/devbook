5b704b8b4578c042e4cfed2ea93dac6383723ead
Nolan VanNurden <nolanvannurden@Nolans-MacBook-Pro.local>
2020-12-17 14:48:10 -0700

    db connected

---
+++ Server/Index.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+const express = require('express');
+const massive = require('massive');
+const session = require('express-session');
+
+
+const {SESSION_SECRET, SERVER_PORT, CONNECTION_STRING} = process.env
+
+const app = express();
+
+app.use(express.json());
+
+
+app.use(session({
+	resave: false,
+	saveUninitialized: true,
+	secret: SESSION_SECRET,
+	cookies: {
+		maxAge: 1000 * 60 * 60 * 24
+	}
+}))
+
+massive({
+	connectionString: CONNECTION_STRING,
+	ssl: {
+		rejectUnauthorized: false
+	}
+}) .then (db => {
+	app.set('db', db)
+	console.log('DB connected')
+}) .catch (err => console.log(err));
+
+
+app.listen(SERVER_PORT, () => console.log(`Listening on ${SERVER_PORT}`));
