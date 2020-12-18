e1d4762a97fb3be4cc7339eeeb3c85135cdac88d
Janie Kim <janie.kim12@gmail.com>
2020-12-18 17:26:13 -0600

    added state

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,29 +1,88 @@
-import React from "react";
-
+import axios from "axios";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 export default function Profile() {
+//   const [state, setState] = useState({
+//     profile_pic: "",
+//     linkedin: "",
+//     portfolio: "",
+//     github: "",
+//     quote: "",
+//   });
+
+//   const addProfile = async (e) => {
+//     e.preventDefault();
+//     const { profile_pic, linkedin, portfolio, github, quote } = state;
+
+//     const history = useHistory();
+
+//     try {
+//       const profile = await axios.post("profile/add", {
+//         profile_pic,
+//         linkedin,
+//         portfolio,
+//         github,
+//         quote,
+//       });
+//       getUser(user.data);
+//       history.push("/profile/user");
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+
+//   const changeHandler = (e) =>
+//     setState({ ...state, [e.target.name]: e.target.value });
+
   return (
     <div className="profile-component">
       <form>
+        {/* onSubmit={(e) => addProfile(e)} */}
         <div className="profile-component-left">
           <div className="profile-pic-input">
-            <input />
+            <input
+            //   className="profile-pic"
+            //   placeholder="Add photo"
+            //   onChange={(e) => changeHandler(e)}
+            //   name="profile_pic"
+            />
           </div>
         </div>
         <div className="profile-component-right">
           <div className="name"></div>
 
           <div className="linkedin-input">
-            <input />
+            <input
+            //   className="profile-linkedin"
+            //   placeholder="LinkedIn Link"
+            //   onChange={(e) => changeHandler(e)}
+            //   name="linkedin"
+            />
           </div>
           <div className="portfolio-input">
-            <input />
+            <input
+            //   className="profile-portfolio"
+            //   placeholder="Portfolio Link"
+            //   onChange={(e) => changeHandler(e)}
+            //   name="portfolio"
+            />
           </div>
           <div className="github-input">
-            <input />
+            <input
+            //   className="profile-github"
+            //   placeholder="Github Link"
+            //   onChange={(e) => changeHandler(e)}
+            //   name="github"
+            />
           </div>
           <div className="quote-input">
-            <input />
+            <input
+            //   className="profile-quote"
+            //   placeholder="Add Quote"
+            //   onChange={(e) => changeHandler(e)}
+            //   name="quote"
+            />
           </div>
         </div>
         <div className="profile-buttons">
@@ -42,3 +101,4 @@ export default function Profile() {
     </div>
   );
 }
+
