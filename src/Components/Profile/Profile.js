979593118a1c949e4333107b2d75a98f9604aa7f
Janie Kim <janie.kim12@gmail.com>
2020-12-21 13:40:42 -0600

    profile update

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,11 +1,9 @@
-import axios from "axios";
 import React, { useState } from "react";
-// import { useHistory } from "react-router-dom";import {connect} from "react-redux"
-import {getUser, loginUser} from "../../redux/userReducer"
-import {connect} from "react-redux"
+import { getUser } from "../../redux/userReducer";
+import { connect } from "react-redux";
+import axios from 'axios'
 
 const Profile = (props) => {
-
   const [state, setState] = useState({
     profile_pic: "",
     linkedin: "",
@@ -14,19 +12,22 @@ const Profile = (props) => {
     quote: "",
   });
 
-  const addProfile = async (e) => {
-    e.preventDefault();
+  const [edit, setEdit] = useState(false);
+
+  const saveEdit = async () => {
+
     const { profile_pic, linkedin, portfolio, github, quote } = state;
+    
 
     try {
-      const profile = await axios.post("profile/add", {
+      const profile = await axios.post("profile/edit", {
         profile_pic,
         linkedin,
         portfolio,
         github,
         quote,
       });
-      getUser();
+      getUser(profile.data);
     props.history.push("/profile/user");
     } catch (error) {
       console.log(error);
@@ -38,79 +39,97 @@ const Profile = (props) => {
 
   return (
     <div className="profile-component">
-      <form onSubmit={(e) => addProfile(e)}>
-        <div className="profile-component-left">
-          <div className="profile-pic-input">
-            <input
-              className="profile-pic"
-              placeholder="Add photo"
-              onChange={(e) => changeHandler(e)}
-              name="profile_pic"
-            />
+      {edit ? (
+        <div>
+          <div className="profile-component-left">
+            <div className="profile-pic-input">
+              <input
+                className="profile-pic"
+                placeholder="Add photo"
+                onChange={(e) => changeHandler(e)}
+                name="profile_pic"
+              />
+            </div>
           </div>
-        </div>
-        <div className="profile-component-right">
-          <div className="name"></div>
+          <div className="profile-component-right">
+            <div className="name"></div>
 
-          <div className="linkedin-input">
-            <input
-              className="profile-linkedin"
-              placeholder="LinkedIn Link"
-              onChange={(e) => changeHandler(e)}
-              name="linkedin"
-            />
-          </div>
-          <div className="portfolio-input">
-            <input
-              className="profile-portfolio"
-              placeholder="Portfolio Link"
-              onChange={(e) => changeHandler(e)}
-              name="portfolio"
-            />
-          </div>
-          <div className="github-input">
-            <input
-              className="profile-github"
-              placeholder="Github Link"
-              onChange={(e) => changeHandler(e)}
-              name="github"
-            />
-          </div>
-          <div className="quote-input">
-            <input
-              className="profile-quote"
-              placeholder="Add Quote"
-              onChange={(e) => changeHandler(e)}
-              name="quote"
-            />
-          </div>
-        </div>
-        <div className="profile-buttons">
-          <div className="profile-edit-button">
-            <button className="profile-edit-button" type="submit">
-              Edit
-            </button>
-          </div>
-          <div className="profile-save-button">
-            <button className="profile-save-button" type="submit">
-              Save
-            </button>
+            <div className="linkedin-input">
+              <input
+                className="profile-linkedin"
+                placeholder="LinkedIn Link"
+                onChange={(e) => changeHandler(e)}
+                name="linkedin"
+              />
+            </div>
+            <div className="portfolio-input">
+              <input
+                className="profile-portfolio"
+                placeholder="Portfolio Link"
+                onChange={(e) => changeHandler(e)}
+                name="portfolio"
+              />
+            </div>
+            <div className="github-input">
+              <input
+                className="profile-github"
+                placeholder="Github Link"
+                onChange={(e) => changeHandler(e)}
+                name="github"
+              />
+            </div>
+            <div className="quote-input">
+              <input
+                className="profile-quote"
+                placeholder="Add Quote"
+                onChange={(e) => changeHandler(e)}
+                name="quote"
+              />
+            </div>
           </div>
         </div>
-      </form>
+      ) : (
+        <div>Profile Page
+          {props.profile_pic}
+
+
+        </div>
+      )}
+
+      {edit ? (
+        <div>
+          <div className="profile-buttons">
+            <div className="profile-edit-button-container">
+              <button className="profile-edit-button" onClick={() => saveEdit(props.profile_pic, props.linkedin, props.portfolio, props.github, props.quote)}>
+                Save
+              </button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <button
+            onClick={() => {
+              setEdit(!edit);
+            }}
+          >
+            Edit
+          </button>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 const mapDispatchToProps = {
-    getUser,
-  };
+  getUser,
+};
 const mapStateToProps = (reduxState) => {
-    const {user, isLoggedIn} = reduxState
-    return {
-        user,
-        isLoggedIn
-    }
-}
+  const { user, isLoggedIn } = reduxState;
+  return {
+    user,
+    isLoggedIn,
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(Profile)
+export default connect(mapStateToProps, mapDispatchToProps)(Profile);
