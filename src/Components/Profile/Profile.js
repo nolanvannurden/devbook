dc897314800ccc3409509c1806e8b0bc6af5cb45
Janie Kim <janie.kim12@gmail.com>
2020-12-29 15:17:17 -0600

    profile form

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,29 +1,40 @@
 import React, { useEffect, useState } from "react";
 import { getUser } from "../../redux/userReducer";
 import { connect } from "react-redux";
-import axios from 'axios'
+import axios from "axios";
+import "./Profile.css";
+import Header from "../Header/Header";
 
 const Profile = (props) => {
+  // const [input, setInput] = useState({profile_pic: '', linkedin: '', portfolio: '', github: '', quote: ''})
+  const [profile_pic, setProfilePic] = useState("");
+  const [linkedin, setLinkedin] = useState("");
+  const [portfolio, setPortfolio] = useState("");
+  const [github, setGithub] = useState("");
+  const [quote, setQuote] = useState("");
 
-  const [input, setInput] = useState('')
   const [edit, setEdit] = useState(false);
-  const [profile, setProfile] = useState([])
-  // const [getProfile, getProfile] = useState([])
+  const [profile, setProfile] = useState([]);
 
   useEffect(() => {
     const getProfile = async () => {
       try {
-        const res = await axios.get("/profile/user")
-        setProfile(res.data)
+        const res = await axios.get("/profile/user");
+        setProfile(res.data);
+      } catch (err) {
+        console.log(err);
       }
-      catch(err){
-        console.log(err)
-      }
-    }
-    getProfile()
-  }, [])
+    };
+    getProfile();
+  }, []);
 
-  const editProfile = async (profile_pic, linkedin, portfolio, github, quote) => {
+  const editProfile = async (
+    profile_pic,
+    linkedin,
+    portfolio,
+    github,
+    quote
+  ) => {
     try {
       const res = await axios.put("/profile/user", {
         profile_pic,
@@ -38,101 +49,180 @@ const Profile = (props) => {
     }
   };
 
-//NOTE: map over the array
+  // const handleChange = e => {
+  //   const {name, value} = e.target;
+  //   setInput({...input, [name]: value})
+  // }
+
+  const uploadedImage = React.useRef(null);
+  const imageUploader = React.useRef(null);
+  const handleImageUpload = (e) => {
+    const [file] = e.target.files;
+    if (file) {
+      const reader = new FileReader();
+      const { current } = uploadedImage;
+      current.file = file;
+      reader.onload = (e) => {
+        current.src = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  };
 
   return (
-    <div className="profile-component">
+    <div>
+      <Header />
 
+      <div className="profile-component">
+        <div>{console.log(profile)}</div>
+        {edit ? (
+          <div className="profile-form">
+            <div className="profile-component-left">
+              <div 
+              className="profile-pic-input"
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center"
+              }}
+              onClick={() => imageUploader.current.click()}
+              >
+                
+                <input
+                  className="profile-pic"
+                  onChange={handleImageUpload}
+                  onChange={(e) => setProfilePic([e.target.files])}
+                  value={profile_pic}
+                  ref={imageUploader}
     
+                  name="profile-pic"
+                  type="file"
+                  accept="image/*"
+                  multiple="false"
+                  ref={imageUploader}
+                  style={{
+                    display: "none"
+                  }}
+                />
+                <img
+                  ref={uploadedImage}
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    border: "none",
+                  }}
+                />
+              </div>
+          
+              </div>
 
-      {edit ? (
-        <div>
-          <div className="profile-component-left">
-            <div className="profile-pic-input">
-              <input
-                className="profile-pic"
-                placeholder="Add photo"
-                value={profile.profile_pic}
-                onChange={(e) => setInput(e.target.value)}
-                name="profile-pic"
-              
-              />
+            <div className="profile-component-right">
+              <div className="name"></div>
+
+              <div className="linkedin-input">
+                <input
+                  className="profile-linkedin"
+                  placeholder="LinkedIn Link"
+                  value={linkedin}
+                  onChange={(e) => setLinkedin(e.target.value)}
+                  name="linkedin"
+                  type="text"
+                />
+              </div>
+              <div className="portfolio-input">
+                <input
+                  className="profile-portfolio"
+                  placeholder="Portfolio Link"
+                  value={portfolio}
+                  onChange={(e) => setPortfolio(e.target.value)}
+                  name="portfolio"
+                  type="text"
+                />
+              </div>
+              <div className="github-input">
+                <input
+                  className="profile-github"
+                  placeholder="Github Link"
+                  value={github}
+                  onChange={(e) => setGithub(e.target.value)}
+                  name="github"
+                  type="text"
+                />
+              </div>
+              <div className="quote-input">
+                <input
+                  className="profile-quote"
+                  placeholder="Add Quote"
+                  value={quote}
+                  onChange={(e) => setQuote(e.target.value)}
+                  name="quote"
+                  type="text"
+                />
+              </div>
             </div>
           </div>
-          <div className="profile-component-right">
-            <div className="name"></div>
+        ) : (
+          <div className="profile">
+            <div className="left-profile">
+              <div className="profile-pic">{profile.profile_pic}</div>
+            </div>
 
-            <div className="linkedin-input">
-              <input
-                className="profile-linkedin"
-                placeholder="LinkedIn Link"
-                value={profile.linkedin}
-                onChange={(e) => setInput(e.target.value)}
-                name="linkedin"
-              />
-            </div>
-            <div className="portfolio-input">
-              <input
-                className="profile-portfolio"
-                placeholder="Portfolio Link"
-                value={profile.portfolio}
-                onChange={(e) => setInput(e.target.value)}
-                name="portfolio"
-              />
-            </div>
-            <div className="github-input">
-              <input
-                className="profile-github"
-                placeholder="Github Link"
-                value={profile.github}
-                onChange={(e) => setInput(e.target.value)}
-                name="github"
-              />
-            </div>
-            <div className="quote-input">
-              <input
-                className="profile-quote"
-                placeholder="Add Quote"
-                value={profile.quote}
-                onChange={(e) => setInput(e.target.value)}
-                name="quote"
-              />
+            <div className="right-profile">
+              <div className="linkedin">
+                <h3>{profile.linkedin}</h3>
+              </div>
+              <br></br>
+              <div className="portfolio">
+                <h3>{profile.portfolio}</h3>
+              </div>
+              <br></br>
+              <div className="github">
+                <h3>{profile.github}</h3>
+              </div>
+              <br></br>
+              <div className="quote">
+                <h3>{profile.quote}</h3>
+              </div>
             </div>
           </div>
-        </div>
-      ) : (
-        <div>
-          <h1>Profile Page</h1>
-          {profile.profile_pic}
-          {profile.linkedin}
-          {profile.portfolio}
-        </div>
-      )}
-
-      {edit ? (
-        <div>
-          <div className="profile-buttons">
-            <div className="profile-edit-button-container">
-              <button className="profile-edit-button" 
-              onClick={() => {
-                editProfile(profile.profile_pic, profile.linkedin, profile.portfolio, profile.github, profile.quote, input) 
-                setEdit(!edit)}}>
-                Save
+        )}
+        <div className="buttons">
+          {edit ? (
+            <div className="form-button">
+              <div className="profile-buttons">
+                <div className="profile-edit-button-container">
+                  <button
+                    className="profile-edit-button"
+                    onClick={() => {
+                      editProfile(
+                        profile_pic,
+                        linkedin,
+                        portfolio,
+                        github,
+                        quote
+                      );
+                      setEdit(!edit);
+                    }}
+                  >
+                    Save
+                  </button>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div>
+              <button
+                onClick={() => {
+                  setEdit(!edit);
+                }}
+              >
+                Edit
               </button>
             </div>
-          </div>
+          )}
         </div>
-      ) : (
-        <div>
-          <button
-            onClick={() => {
-              setEdit(!edit);
-            }}
-          >
-            Edit
-          </button>
-        </div>
-      )}
+      </div>
     </div>
   );
 };
