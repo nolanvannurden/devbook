2398ad3b33bcb97ef989cb9ac375f60262c7c7ca
Janie Kim <janie.kim12@gmail.com>
2020-12-29 17:49:05 -0600

    profile form functionality

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -110,7 +110,7 @@ const Profile = (props) => {
                   style={{
                     width: "100%",
                     height: "100%",
-                    border: "none",
+
                   }}
                 />
               </div>
