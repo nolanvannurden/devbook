fcceb4384ba903a222d182dc523f23ba5db2f96f
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2020-12-29 10:58:53 -0800

    css and filter functionality

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -3,14 +3,15 @@ import {connect} from 'react-redux';
 import {getUser} from '../../redux/userReducer';
 import Axios from 'axios';
 import {useState, useEffect} from 'react';
-import githubLogo from './img/GitHub_logo.png';
+import githubLogo from './img/GitHub-logo.png';
 import linkedinLogo from './img/linkedin.png';
 import websiteLogo from './img/website.png';
 import Header from '../Header/Header';
 const Dashboard = (props) => {
 
   const [users, setUsers] = useState([]);
-  const [profiles, setProfiles] = useState([])
+  const [profiles, setProfiles] = useState([]);
+  const [nameFilter, setNameFilter] = useState('');
 
   useEffect(() => {
     
@@ -31,20 +32,30 @@ const Dashboard = (props) => {
 
 
   
-
-
-  const allUsers = users.map(user => {
-
+  
+  
+  
+  const allUsersFiltered = users.filter( (user) => {
+      if(nameFilter.length < 2){
+        return true
+      } else {
+        return user.first_name.includes(nameFilter)
+      }
+    })
+    .map(user => {
+    
     const [profile] = profiles.filter( profile => profile.user_id === user.user_id);
-
+    
     console.log(`[profile] for user ${user.first_name}`, profile);
+    
 
     if (profile) {
       return (
       <div className="container">
       <div className='box'>
         <img src={profile.profile_pic}/>
-        <h2>finished profile for {user.first_name}</h2>
+        <h2>{user.first_name} {user.last_name}</h2>
+        <section className="logos">
         <a href={profile.github} target='_blank'>
           <img src={githubLogo} style={{height:'30px', width:'30px'}}/>
         </a>
@@ -54,7 +65,7 @@ const Dashboard = (props) => {
         <a href={profile.portfolio} target='_blank'>
           <img src={websiteLogo} style={{height:'30px', width:'30px'}}/>
         </a>
-        
+        </section>
         <h4>{profile.quote}</h4>
         </div>
         </div>
@@ -63,7 +74,9 @@ const Dashboard = (props) => {
       return (
         <div className="container">
           <div className="box">
-            <li>unfinished profile for {user.first_name}</li>
+            <li>{user.first_name}</li>
+            <li>{user.last_name}</li>
+            <li>{user.email}</li>
        </div> 
        </div>
         )
@@ -71,13 +84,19 @@ const Dashboard = (props) => {
  
   })
 
+
     return(
 
 
         <div>
           <Header/>
+
           <p>Welcome, {props.user.email}</p>
-        <div className="container container-container">{allUsers}</div>
+          <p>Search by name: 
+            <input type="text"
+              onChange={(e) => {setNameFilter(e.target.value)}}
+        /></p>
+        <div className="container container-container">{allUsersFiltered}</div>
         </div>
     )
 }
