8ac398a84fbb86542f0b2154536e180d598a8d22
Nolan VanNurden <nolanvannurden@Nolans-MacBook-Pro.local>
2020-12-30 09:19:39 -0700

    formatted some files

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,6 @@
-import {applyMiddleware, createStore} from 'redux';
-import userReducer from './userReducer'
-import promiseMiddleware from 'redux-promise-middleware'
+import { applyMiddleware, createStore } from "redux";
+import userReducer from "./userReducer";
+import promiseMiddleware from "redux-promise-middleware";
 
-
-
-export default createStore(userReducer, applyMiddleware(promiseMiddleware))
+export default createStore(userReducer, applyMiddleware(promiseMiddleware));
 // export default createStore(reducer, applyMiddleware(promiseMiddleware))
