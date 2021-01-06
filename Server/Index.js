a1e6417f3377772be74dd76eba35eb1ad2826819
Janie Kim <janie.kim12@gmail.com>
2021-01-06 16:04:12 -0600

    .env

--- Server/Index.js
+++ Server/Index.js
@@ -17,9 +17,6 @@ app.use(express.json());
 
 app.use(express.static(__dirname + '/../build'))
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build/index.html'))
-})
 
 app.use(session({
 	resave: false,
